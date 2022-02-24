b249575bc359d08d29ed4869e6bbc169f703e438
Avelino <avelino5872@gmail.com>
2022-02-24 19:00:08 +0100

    Solución propuesta Challenge 8

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge8Solved.kt
@@ -0,0 +1,33 @@
+package com.mouredev.weeklychallenge2022
+
+/*
+ * Reto #8
+ * DECIMAL A BINARIO
+ * Fecha publicación enunciado: 18/02/22
+ * Fecha publicación resolución: 02/03/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa se encargue de transformar un número decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+fun main() {
+    print(toBinary(13))
+}
+
+private fun toBinary(decimalNumber: Int): String {
+    val binaryNumber = StringBuilder();
+    var quotient = decimalNumber
+    do {
+        val remainder = quotient % 2
+        quotient /= 2
+        binaryNumber.insert(0, remainder)
+    } while (quotient != 0)
+    return binaryNumber.toString()
+}
